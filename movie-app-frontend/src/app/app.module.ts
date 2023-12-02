f533d1133528275f7641920ca36411a7c24b5a1d
Ricardo A. Abreu <ricardo.ap.abreu@outlook.com>
2023-12-02 14:19:17 -0300

    Refactor: adjusted estructure to hold BE and FE

---
+++ movie-app-frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { ReviewComponent } from './components/review/review.component';
+import { PosterComponent } from './components/poster/poster.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    MovieComponent,
+    ReviewComponent,
+    PosterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ movie-app-frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    appName: "Teste K2",
+    version: "1.0.0",
+    backendUrl: "http://www.omdbapi.com/?i=tt3896198&apikey=751f3015",    
+    redirectOnExitingApp: "login",
+    redirectToHome: "home",
+  };
